ee587b17425afa05daa6a6bb2f152c1d91469439
Alex Persaud <alex_persaud01@yahoo.com>
2017-03-08 13:30:01 -0500

    Add code for new employee questions

---
+++ ruby/vampires.rb
@@ -0,0 +1,8 @@
+puts "What is your name?"
+ww_name=gets.chomp
+puts "How old are you? What year were you born?"
+ww_age_year=gets.chomp
+puts "Our cafeteria serves garlic bread. Should we order some for you?"
+ww_garlic=gets.chomp
+puts "Would you like to enroll in the company's health insurance?"
+ww_health=gets.chomp

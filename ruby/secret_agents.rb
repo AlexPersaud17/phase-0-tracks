767f256e094dd1c94350c8e2b45527823ee6cfc6
Alex Persaud <alex_persaud01@yahoo.com>
2017-03-09 15:18:02 -0500

    Add user input

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -11,15 +11,19 @@ def encrypt(password)
 		if password[index]==" "
 			print " "
 			index+=1
+		elsif password[index]=="z"
+			print "a"
+			index+=1
+		else
+			print password[index].next
+			index+=1
 		end
-		print password[index].next
-		index+=1
 	end
 	puts " "
 end
 
-puts"What's your password?"
-encrypt(gets.chomp)
+#puts"What's your password?"
+#encrypt(gets.chomp)
 
 =begin
 DECRYPT
@@ -39,6 +43,9 @@ def decrypt(encrypted_password)
 				if encrypted_password[e_index]==" "
 					print " "
 					e_index+=1
+				elsif encrypted_password[e_index]=="a"
+					print "z"
+					e_index+=1
 				else
 					print decrypter[d_index-1]
 				end
@@ -49,5 +56,25 @@ def decrypt(encrypted_password)
 	end
 end
 
-puts "What's your encrypted password?"
-decrypt(gets.chomp)
+#puts "What's your encrypted password?"
+#decrypt(gets.chomp)
+
+
+decrypt(encrypt("swordfish"))
+
+=begin
+-encrypt of decrypt?
+-ask for password
+-performs necessary operation
+-print results
+=end
+
+puts "Would you like to encrypt or decrypt?"
+user_input=gets.chomp.downcase
+if user_input == "encrypt"
+	puts "What would you like to encrypt?"
+	encrypt(gets.chomp)
+elsif  user_input == "decrypt"
+	puts "What would you like to decrypt?"
+	decrypt(gets.chomp)
+end

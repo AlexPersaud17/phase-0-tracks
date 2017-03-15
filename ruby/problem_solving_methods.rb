45a3509da0f202d706cafa572e1fde9c0c51dc99
Alex Persaud <alex_persaud01@yahoo.com>
2017-03-15 16:55:57 -0400

    Calculate Fibonacci number, 5.6, release 1

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -1,3 +1,6 @@
+# Release 0
+
+=begin
 int_array = [12, 4564, 78, 9, 0, 45]
 def search(int_array, value)
 	index=0
@@ -17,11 +20,23 @@ def search(int_array, value)
 	end
 end
 
-
-
 puts "Value?"
 input = gets.chomp.to_i
 search(int_array, input)
+=end
 
 
+# Release 1
 
+fib = [0, 1]
+puts "How many values?"
+limit = gets.chomp.to_i
+i = 0
+while i < limit - 2
+	fib << fib[-1] + fib[-2]
+	i+=1
+end
+p fib
+# Testing given value with value of last index
+test=218922995834555169026
+p fib.fetch(-1)==test

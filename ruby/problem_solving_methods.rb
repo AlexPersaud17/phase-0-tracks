0b00c359ad7ffb403e229d24d92995cc91763e33
Alex Persaud <alex_persaud01@yahoo.com>
2017-03-15 17:09:06 -0400

    Sort an array

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -1,6 +1,5 @@
 # Release 0
 
-=begin
 int_array = [12, 4564, 78, 9, 0, 45]
 def search(int_array, value)
 	index=0
@@ -23,7 +22,7 @@ end
 puts "Value?"
 input = gets.chomp.to_i
 search(int_array, input)
-=end
+
 
 
 # Release 1
@@ -40,3 +39,33 @@ p fib
 # Testing given value with value of last index
 test=218922995834555169026
 p fib.fetch(-1)==test
+
+# Release 2
+=begin
+
+Take's first value of array
+Compare to following value of array
+Sort to put larger number before
+Repeat until all values of the array have been tested
+
+=end
+
+def bubble_sort(array)
+  n = array.length
+  loop do
+    swapped = false
+
+    (n-1).times do |i|
+      if array[i] < array[i+1]
+        array[i], array[i+1] = array[i+1], array[i]
+        swapped = true
+      end
+    end
+
+  break if not swapped
+  end
+
+  array
+end
+
+p bubble_sort(fib)

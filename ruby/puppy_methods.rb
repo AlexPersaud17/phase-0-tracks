3155b17930d237d25a5ca6adeb64fdeb8302570c
Alex Persaud <alex_persaud01@yahoo.com>
2017-03-20 14:16:37 -0400

    Add behavior to the class

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -5,5 +5,31 @@ class Puppy
     toy
   end
 
+  def speak(num)
+  	num.times do
+  		puts "Woof!"
+  	end
+  end
+
+  def roll_over
+  	puts "Roll over!"
+  end
+
+  def dog_years(human_years)
+  	dog_years = human_years * 7
+  	dog_years
+  	p dog_years
+  end
+
+  def play_dead
+  	puts "*dies*"
+  end
 end
 
+
+rocky = Puppy.new
+rocky.fetch("ball")
+rocky.speak(5)
+rocky.roll_over
+rocky.dog_years(9)
+rocky.play_dead

4e5316578f1a6d1e0f3e2a5de7b501c0e2234a50
Alex Persaud <alex_persaud01@yahoo.com>
2017-03-21 12:22:02 -0400

    Write a simple module

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,4 +1,12 @@
 module Shout
+	def self.yelling_angrily(words)
+		words + "!!!" + " :("
+	end
 
+	def self.yelling_happily(words)
+		words + "!!!" + " :)"
+	end
 
 end
+
+puts Shout.yelling_happily("LOUD NOISES")

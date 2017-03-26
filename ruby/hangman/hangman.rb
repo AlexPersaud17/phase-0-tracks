0e33924a9eba8bd7f8b9fea027d510c88b47c1f3
Alex Persaud <alex_persaud01@yahoo.com>
2017-03-26 15:01:27 -0400

    Add method for choosing

--- ruby/hangman/hangman.rb
+++ ruby/hangman/hangman.rb
@@ -1,12 +1,13 @@
 class Hangman
-	attr_reader :letters, :correct_guess, :non_space_chars, :spaces
-
 
 	def initialize(secret_word)
+		@secret_word = secret_word
 		@letters = secret_word.downcase.split('')
 		@non_space_chars = 0
 		@placeholder = "_ "
 		@spaces = []
+		@winner = true
+		@letters_chosen = []
 
 		@letters.each do |letter|
 			if letter != " "
@@ -15,22 +16,28 @@ class Hangman
 				@spaces << "   "
 			end
 		end
+		
     	@spaces.each do |letter|
       		if letter != "   "
         	@non_space_chars += 1
       		end
     	end
+    	@guess_count = @non_space_chars
+
 		puts "You only have #{@non_space_chars} guesses, so choose wisely!"
 		@spaces.each do |space|
 			print space
 		end
 		puts ""
+		guessing_loop
 	end
 
 
 	def guess(choice)
+	  @letters_chosen << choice
 		@letter_location = @letters.each_index.select { |letter| @letters[letter] == choice }
 		@letters.each do |letter|
+		  
 			if letter == choice
 				@letter_location.each do |index|
 					@spaces[index] = letter
@@ -44,24 +51,52 @@ class Hangman
 		@spaces.each do |letter|
 			print letter
 		end
+		if !@correct_guess
+				@guess_count -= 1
+		end
+		if @guess_count == 0
+				@winner = false
+		end
 		@spaces
 	end
 
 
 	def check_win
-		@spaces.include?(@placeholder)
+	  puts ""
+		if @spaces.include?(@placeholder) && @winner
+		  guessing_loop
+		elsif @winner
+		  puts "You got the secret word '#{@secret_word}'! Well done! Do with it what you must."
+		else
+		  puts "Wow you had #{@secret_word.length} tries and you STILL didn't get it? The answer was '#{@secret_word}' by the way..."
+		end
+	end
+
+
+
+	def guessing_loop
+		puts ""
+		if @guess_count != 1
+			puts "#{@guess_count} guesses left!"
+		else
+			puts "#{@guess_count} guess left!"
+		end
+
+		puts "Guess please."
+		@winner = true
+		user_guess = gets.chomp
+		if @letters_chosen.include?(user_guess)
+		  puts "You chose this already, go again."
+		  @spaces.each do |letter|
+			  print letter
+		  end
+		else
+		  guess(user_guess)
+		end
+		check_win
 	end
 
 end
-
-
-
-
-
-
-# COMMENTED OUT FOR TESTING
-=begin
-
 
 
 # PICK YOUR SUPER SECRET WORD OR PHRASE!
@@ -69,53 +104,8 @@ puts "Please enter your super secret word or phrase:"
 secret_word = gets.chomp
 
 # OR UNCOMMENT LINE BELOW TO HAVE A SECRET WORD CHOSEN FROM THE LIST !
-#random_words = ["centaur", "shocking", "power", "flirt", "falling", "facade", "clover", "pink", "warmth", "angriest", "weapon", "orphanage", "cruel", "careless", "event", "melody", "coincidence", "amplitude", "correlation", "application", "cuddle", "blasting", "subsonic"]
-#secret_word = random_words[rand(random_words.length - 1)]
-
+random_words = ["centaur", "shocking", "power", "flirt", "falling", "facade", "clover", "pink", "warmth", "angriest", "weapon", "orphanage", "cruel", "careless", "event", "melody", "coincidence", "amplitude", "correlation", "application", "cuddle", "blasting", "subsonic"]
+#secret_word = random_words.sample
 
 
 new_game = Hangman.new(secret_word)
-
-
-
-puts ""
-guess_count = new_game.non_space_chars
-winner = true
-
-loop do
-	puts ""
-
-	if guess_count != 1
-		puts "#{guess_count} guesses left!"
-	else
-		puts "#{guess_count} guess left!"
-	end
-
-	puts "Guess please."
-	winner = true
-	new_game.guess(gets.chomp)
-	new_game.check_win
-	break if not new_game.check_win
-	puts ""
-
-	if !new_game.correct_guess
-		guess_count -= 1
-	end
-
-	if guess_count == 0
-		winner = false
-		break
-	end
-end
-
-puts ""
-if winner
-	puts "You got the secret word '#{secret_word}'! Well done! Do with it what you must."
-else
-	puts "Wow you had #{secret_word.length} tries and you STILL didn't get it? The answer was '#{secret_word}' by the way..."
-end
-
-
-
-
-=end

35c9648daee0213d8391ebcbb68b32733ed8e236
Alex Persaud <alex_persaud01@yahoo.com>
2017-03-28 18:20:50 -0400

    Determine what each method does

--- ruby/gps6/my_solution.rb
+++ ruby/gps6/my_solution.rb
@@ -4,18 +4,20 @@
 # We spent [#] hours on this challenge.
 
 # EXPLANATION OF require_relative
-#
-#
+# Locate the file that needs to be linked ie. data for this class
+
 require_relative 'state_data'
 
 class VirusPredictor
 
+  # Initializing instance variables to be used throughout the class
   def initialize(state_of_origin, population_density, population)
     @state = state_of_origin
     @population = population
     @population_density = population_density
   end
 
+  # Method that runs the other methods
   def virus_effects  
     predicted_deaths(@population_density, @population, @state)
     speed_of_spread(@population_density, @state)
@@ -23,18 +25,19 @@ class VirusPredictor
 
   private
 
+  # 
   def predicted_deaths(population_density, population, state)
     # predicted deaths is solely based on population density
     if @population_density >= 200
-      number_of_deaths = (@population * 0.4).floor
+      number_of_deaths = (@population * 0.4)
     elsif @population_density >= 150
-      number_of_deaths = (@population * 0.3).floor
+      number_of_deaths = (@population * 0.3)
     elsif @population_density >= 100
-      number_of_deaths = (@population * 0.2).floor
+      number_of_deaths = (@population * 0.2)
     elsif @population_density >= 50
-      number_of_deaths = (@population * 0.1).floor
+      number_of_deaths = (@population * 0.1)
     else
-      number_of_deaths = (@population * 0.05).floor
+      number_of_deaths = (@population * 0.05)
     end
 
     print "#{@state} will lose #{number_of_deaths} people in this outbreak"

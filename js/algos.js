bbb0ce716233f8cecedf0abf682f09a95a5728dd
Alex Persaud <alex_persaud01@yahoo.com>
2017-03-28 15:38:40 -0400

    Generate random words and choose the longest

--- js/algos.js
+++ js/algos.js
@@ -1,3 +1,11 @@
+// Release 0
+// Check the length of each word in the list
+// Have a variable that stores the first word in the list
+// 	If following word has more letters, update the variable to store that new word
+// 	If not, test the word after
+// Longest word should be stored in variable
+// Display that variable
+
 var phrases1 = ["long phrase","longest phrase","longer phrase", "phrase"];
 var phrases2 = ["super", "cali", "fragilistic", "expi", "ali", "docious"];
 var phrases3 = ["anti", "disestablishment", "arianism"]
@@ -18,6 +26,12 @@ function longPhraseChecker (phraseArray){
 // console.log(longPhraseChecker(phrases2));
 // console.log(longPhraseChecker(phrases3));
 
+
+// Release 1
+// Create a list of the keys in the objects
+// Test the same key of each object to see if they store the same value
+// If so, display true
+
 function matching(object1, object2){
 	var objectKeys = [];
 	for(var keys in object1){ objectKeys.push(keys) }
@@ -33,4 +47,24 @@ function matching(object1, object2){
 // keyValue2 = {name: "Tamir", age: 54}
 // console.log(matching(keyValue1, keyValue2));
 
+function randomWordCreator(wordCount){
+	randomWords = [];
+	for(var count = 1; count <= wordCount; count++){
+		var randWord = "";
+		var allChars = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+		var letter;
+		var howManyLetters = 10;
+		var randLetterCount = Math.floor((Math.random() * howManyLetters) + 1);
+
+		for(letter = 1; letter <= randLetterCount; letter++){
+			randWord += allChars[(Math.floor(Math.random() * allChars.length))];
+		}
+		randomWords.push(randWord);
+	}
+	return randomWords;
+
+}
+var howManyWords = 10
+console.log(randomWordCreator(howManyWords));
+console.log("\nThe longest word in this array is '" + longPhraseChecker(randomWords) + "'.");
 

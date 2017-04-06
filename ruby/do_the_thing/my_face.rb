490c692446aa33f229d34632d37c862967f35a56
Alex Persaud <alex_persaud01@yahoo.com>
2017-04-06 13:23:30 -0400

    Add friend and page variation

--- ruby/do_the_thing/my_face.rb
+++ ruby/do_the_thing/my_face.rb
@@ -1,4 +1,4 @@
-# WELCOME TO MYFACE!
+# WELCOME TO MYFACE! (Get it? MySpace + Facebook???)
 # Created by Alex Persaud
 
 require 'sqlite3'
@@ -33,34 +33,60 @@ db.execute(create_friends_table)
 db.execute(create_fan_pages_table)
 db.execute(create_pages_liked_table)
 
-# Populate tables with MyFace users and related information
+# Populate table with MyFace users and related information
 def create_friends(db, name, status, check_in)
 	db.execute('INSERT INTO friends (name, status, check_in) VALUES	(?, ?, ?)', [name, status, check_in])
 end
 
+# Create MyFace users of many different backgrounds
 how_many_friends = rand(10) + 10
 how_many_friends.times do
-	create_friends(db, Faker::HarryPotter.character, Faker::HarryPotter.quote, Faker::HarryPotter.location)
+	type_of_friend = rand(5) + 1
+	case type_of_friend
+	when 1
+		create_friends(db, Faker::HarryPotter.character, Faker::HarryPotter.quote, Faker::HarryPotter.location)
+	when 2
+		create_friends(db, Faker::Friends.character, Faker::Friends.quote, Faker::Friends.location)
+	when 3
+		create_friends(db, Faker::LordOfTheRings.character, Faker::ChuckNorris.fact, Faker::LordOfTheRings.location)
+	when 4
+		create_friends(db, Faker::StarWars.character, Faker::StarWars.quote, Faker::StarWars.planet)
+	when 5
+		create_friends(db, Faker::GameOfThrones.character, Faker::ChuckNorris.fact, Faker::GameOfThrones.city)
+	end
 end
 
-# Populate tables with MyFace fan page names
+# Populate table with MyFace fan page names
 def create_fan_pages(db, name)
 	db.execute('INSERT INTO fan_pages (name) VALUES	(?)', [name])
 end
 
+# Create various MyFace fan pages
 how_many_fan_pages = rand(15) + 15
 how_many_fan_pages.times do
-	random_pages = [Faker::Vehicle.unique.manufacture, Faker::University.unique.name, Faker::Superhero.unique.name, Faker::Pokemon.unique.name, Faker::Company.unique.name, Faker::Address.unique.country, Faker::App.unique.name]
+	random_pages = [
+		Faker::Vehicle.unique.manufacture, 
+		Faker::University.unique.name, 
+		Faker::Superhero.unique.name, 
+		Faker::Pokemon.unique.name, 
+		Faker::Company.unique.name, 
+		Faker::Address.unique.country, 
+		Faker::App.unique.name, 
+		Faker::Book.unique.title, 
+		Faker::Beer.unique.name, 
+		Faker::Team.unique.name
+	]
 	create_fan_pages(db, random_pages.sample)
 end
 
-# Create many-to-many relationships between MyFace users and fan pages
+# Populate table with relationships between MyFace users and fan pages
 def pages_liked(db, friend_id, fan_id)
 	db.execute('INSERT INTO pages_liked (friend_id, fan_id) VALUES	(?, ?)', [friend_id, fan_id])
 end
 
-how_many_friends.times do |friend| # zero indexed; x+1
-	total_likes = rand(5)+1
+# Randomizes how many pages each MyFace user likes and what they are
+how_many_friends.times do |friend|
+	total_likes = rand(4)+1
 	total_likes.times do
 		pages_liked(db, friend+1, rand(how_many_fan_pages)+1)
 	end
@@ -87,53 +113,64 @@ who_likes_what = db.execute(who_likes_what_sql)
 list_friends = db.execute(list_friends_sql)
 news_feed = db.execute(news_feed_sql)
 
-new_status = Faker::Friends.quote
-new_check_in = Faker::Friends.location
+my_status = Faker::Friends.quote
+my_check_in = Faker::Friends.location
 
 # USER INTERFACE
 puts "Welcome to MyFace!"
 puts "Please enter your username to log in."
 username = gets.chomp.capitalize
+puts "\n"
 puts "Welcome back #{username}!"
+puts "\n"
 puts "What would you like to do?"
 
 
 # Loops until MyFace user 'logs out'
 loop do
-	puts "You can enter 'My Profile', 'Status Update', 'Check In', 'News Feed', 'View Friends', or 'Logout' when you're done!"
+	puts "\n"
+	puts "You can enter 'My Profile', 'Status Update', 'Check In', 'News Feed', 'My Friends', or 'Logout' when you're done!"
 	choice = gets.chomp.downcase
 	case choice
 	when "my profile"
+		puts "\n"
 		puts "Name: #{username}"
-		puts "Last Status Update: '#{new_status}'"
-		puts "Last Check In: #{new_check_in}"
+		puts "Last Status Update: '#{my_status}'"
+		puts "Last Check In: #{my_check_in}"
 		puts "You have #{how_many_friends} friends!"
 	when "status update"
+		puts "\n"
 		puts "Press 'enter' to update!"
-		new_status = gets.chomp
+		my_status = gets.chomp
 	when "check in"
+		puts "\n"
 		puts "Press 'enter' to update!"
-		new_check_in = gets.chomp
+		my_check_in = gets.chomp
 	when "news feed"
+		puts "\n"
 		puts "Popular pages!"
 		who_likes_what.each do |who, what|
-			puts "#{who} likes #{what}."
+			puts "--#{who} likes #{what}."
 		end
+		puts "\n"
 		puts "What your friends are up to!"
 		news_feed.each do |name, status, place|
-			puts "#{name} is at #{place} and says '#{status}'"
+			puts "--#{name} is at #{place} and says '#{status}'"
 		end
-	when "view friends"
+	when "my friends"
+		puts "\n"
 		list_friends.each do |friend|
-			puts "#{friend[0]}"
+			puts "--#{friend[0]}"
 		end
 	when "logout"
 		break
 	else
+		puts "\n"
 		puts "Sorry, that is not a valid entry."
 	end
 end
 
+puts "\n"
 puts "Thank you for using MyFace!"
 db.execute('drop table friends')
 db.execute('drop table fan_pages')

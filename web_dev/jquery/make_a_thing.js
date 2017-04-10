91fe93e9e831e1ba566bc20cb6bc8e8e6f5315f1
Alex Persaud <alex_persaud01@yahoo.com>
2017-04-10 15:50:51 -0400

    Make mario sprite move

--- web_dev/jquery/make_a_thing.js
+++ web_dev/jquery/make_a_thing.js
@@ -1,6 +1,22 @@
 $(document).ready(function(){
-	console.log("jquery works");
-    $("#chameleon").click(function(){
-        $("p").hide();
-    });
+        $(document).keydown(function(key) {
+        switch(key.which) {
+			// Left Arrow
+			case 37:
+				$('#mario').animate({left: "-=50px"}, 30);
+				break;
+			// Up Arrow
+			case 38:
+				$('#mario').animate({top: "-=50px"}, 30);
+				break;
+			// Right Arrow
+			case 39:
+				$('#mario').animate({left: "+=50px"}, 30);
+				break;
+			// Down Arrow
+			case 40:
+				$('#mario').animate({top: "+=50px"}, 30);
+				break;
+		}
+	});
 });

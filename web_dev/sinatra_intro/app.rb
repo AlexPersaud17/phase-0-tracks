f497b2731e8a56626ae2ee0fa00fdd368730fe5c
Alex Persaud <alex_persaud01@yahoo.com>
2017-04-12 14:17:26 -0400

    Add routes to web app

--- web_dev/sinatra_intro/app.rb
+++ web_dev/sinatra_intro/app.rb
@@ -1,5 +1,6 @@
 # require gems
-require 'sinatra'
+require "sinatra"
+require "sinatra/reloader" if development?
 require 'sqlite3'
 
 db = SQLite3::Database.new("students.db")
@@ -44,3 +45,35 @@ get '/students/:id' do
   student = db.execute("SELECT * FROM students WHERE id=?", [params[:id]])[0]
   student.to_s
 end
+
+# A /contact route that displays an address
+get '/contact' do
+  "1600 Pennsylvania Ave"
+end
+
+# A /great_job route that can take a person's name as a query parameter (not a route parameter) and say "Good job, [person's name]!". If the query parameter is not present, the route simply says "Good job!"
+get '/great_job' do
+  name = params[:name]
+  if name
+    "Good job, #{name}!"
+  else
+    "Good job!"
+  end
+end
+
+# A route that uses route parameters to add two numbers and respond with the result.
+get '/:num1/plus/:num2' do
+  num1 = params[:num1].to_i
+  num2 = params[:num2].to_i
+  result = num1 + num2
+  "#{num1} plus #{num2} equals #{result}"
+end
+
+# Make a route that allows the user to search the database in some way -- maybe for students who have a certain first name, or some other attribute.
+# http://localhost:4567/Twila%20Ortiz
+get '/:student' do
+  student = params[:student]
+  student_info=db.execute('SELECT * FROM students WHERE name=?', [student])
+  student_info.to_s
+end
+
